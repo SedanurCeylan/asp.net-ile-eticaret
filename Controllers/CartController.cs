a6799d064c648c7710f1c796e67df29feec564fb
SedanurCeylan <sdnrcyln2@gmail.com>
2025-10-28 17:56:32 +0300

    sepete ekle

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,13 +1,60 @@
+using System.Threading.Tasks;
+using e_ticaret_proje.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace e_ticaret_proje.Controllers;
 
+[Authorize]
 public class CartController : Controller
 {
-    [Authorize]
-    public ActionResult Index()
+    private readonly DataContext _context;
+
+    public CartController(DataContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddToCart(int urunId, int miktar=1)
+    {
+        var customerId = User.Identity?.Name;
+
+        var cart = await _context.Carts.Include(i => i.CartItems)
+                                        .Where(i => i.CustomerId == customerId)
+                                        .FirstOrDefaultAsync();
+
+        if (cart == null)
+        {
+            cart = new Cart
+            {
+                CustomerId = customerId!
+            };
+            _context.Carts.Add(cart);
+        }
+
+        var item = cart.CartItems.Where(i => i.UrunId == urunId).FirstOrDefault();
+
+        if (item != null)
+        {
+            item.Miktar += 1;
+            //daha önce eklenmiş
+        }
+        else
+        {
+            cart.CartItems.Add(new CartItem
+            {
+                UrunId= urunId,
+                Miktar=miktar
+            });
+
+            //ilk defa ekleniyor
+        }
+
+        await _context.SaveChangesAsync();
+
+
+        return RedirectToAction("Index","Home");
     }
 }
